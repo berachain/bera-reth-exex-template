c22caae7376bcec109650df99813c6bf7712d6d0
Rez <rez@berachain.com>
2025-09-15 09:35:33 +1000

    working

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,15 @@ use clap::Parser;
 use reth::CliRunner;
 use reth::cli::Cli;
 use reth_cli_commands::node::NoArgs;
-use reth_evm::EthEvmFactory;
 use reth_node_builder::NodeHandle;
 use reth_tracing::tracing::info;
 use std::sync::Arc;
+use bera_reth::evm::BerachainEvmFactory;
 
 fn main() -> eyre::Result<()> {
     let cli_components_builder = |spec: Arc<BerachainChainSpec>| {
         (
-            BerachainEvmConfig::new_with_evm_factory(spec.clone(), EthEvmFactory::default()),
+            BerachainEvmConfig::new_with_evm_factory(spec.clone(), BerachainEvmFactory::default()),
             BerachainBeaconConsensus::new(spec),
         )
     };
